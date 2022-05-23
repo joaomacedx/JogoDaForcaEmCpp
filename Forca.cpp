5db4a868f48674ea669c4f737a9af1623c37ce53
João Macêdo <99701121+joaomacedx@users.noreply.github.com>
2022-05-23 17:57:55 -0300

    Implementando regras do jogo

--- Forca.cpp
+++ Forca.cpp
@@ -1,14 +1,77 @@
 #include <iostream>
 #include <locale.h>
+#include <cstdlib>
+#include <string>
+#include <map>
+#include <vector>
 
 using namespace std;
+const string PALAVRA_SECRETA = "MELANCIA";
+map<char, bool> chutou;
+vector<char> chutesErrados;
+
+bool letraExiste(char chute)
+{
+	 for (char letra : PALAVRA_SECRETA)
+	 {
+	 	 if (chute == letra)
+	 	{
+	 	 	return true;
+	 	}
+	 }
+ 	return false;
+}
+
+
+
 int main ()
 {
-	setlocale (LC_ALL, "portuguese");
+	 setlocale (LC_ALL, "portuguese");
+	 cout << "************************"<<endl;
+	 cout << "*     JOGO DA FORCA    *" <<endl;
+	 cout << "************************"<<endl;
+	 cout <<endl;
+	 bool naoAcertou = true;
+	 bool naoEnforcou = true;
+		
 	
-	string palavraSecreta = "MELANCIA";
-	
-	cout << palavraSecreta <<endl;
-	
+	 while(naoAcertou && naoEnforcou)
+	 {	 
+	     cout << "Chutes errados: ";
+	     for (char letra : chutesErrados)
+		 {
+	     	cout << letra << " ";
+		 }
+		 cout << endl;
+	     for(char letra : PALAVRA_SECRETA)
+		 {
+	 	 	if (chutou[letra])
+		 	{
+	 	 		cout << letra << " "; 
+	     	}
+	     	else 
+	     	{
+		     	cout << "_ ";
+		 	}
+	     } 
+	     cout << endl;
+	     
+	     
+		 char chute;
+		 cout << "Chute uma letra maiúscula para verificar se ela existe na Palavra Secreta" <<endl;
+		 cin >> chute;
+		 chutou[chute] = true;
+  	      	if (letraExiste(chute))
+		 	{
+		 		 cout << "Você acertou! Seu chute está na palavra!" <<endl;
+		 		 
+		 	}
+		 	else 
+			 {
+			 	 cout << "Você errou! Seu chute não está na palavra!" <<endl;
+			 	 chutesErrados.push_back(chute);
+			 }
+ 	
+	 }
 	
 }
